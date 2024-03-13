1890580639c5240c93e9c4253b7c5fb1a2f65028
Emilio Aranda <ara.emi44@gmail.com>
2024-03-13 22:44:04 +0100

    hola project

---
+++ slash.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+display_help() {
+  echo "Usage: $0 [-c value]"
+  echo "  -c   Specify the content to display"
+  echo "  --help   Display this help message"
+  exit 0
+}
+
+# Check for --help option
+if [[ "$1" == "--help" ]]; then
+  display_help
+fi
+while getopts ":c:" opt; do
+  case $opt in
+    c)
+      arg_c="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ $arg_c ]]; then
+  columns=$(tput cols) # Screen width
+  partLength=$((($columns - ${#arg_c}) / 2 - 1))
+
+  part1=$(printf '=%.0s' $(seq 1 $partLength))
+  part2=$(printf '=%.0s' $(seq 1 $partLength))
+
+  echo "$part1 $arg_c $part2"
+  exec $arg_c
+else
+  echo $(printf '=%.0s' $(seq 1 $(tput cols)))
+fi
+
