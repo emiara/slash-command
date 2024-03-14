763092b78bffa07ebda495ff219aae13fad48a0d
Emilio Aranda <ara.emi44@gmail.com>
2024-03-14 01:59:19 +0100

    max length for banner text

--- slash.sh
+++ slash.sh
@@ -1,8 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+pretty_shorten_text() {
+    local text="$1"
+    local columns="$2"
+
+    # Calculate max length as 80% of terminal width
+    local max_length=$((columns * 80 / 100))
+
+    if [ ${#text} -gt $max_length ]; then
+        echo "${text:0:$max_length}..."
+    else
+        echo "$text"
+    fi
+}
+
 
 display_help() {
   echo "Usage: $0 [-c value]"
-  echo "  -c   Specify the content to display"
+  echo "  -c   Run command after banner"
+  echo "  -t   Specify the content to display"
   echo "  --help   Display this help message"
   exit 0
 }
@@ -11,10 +27,14 @@ display_help() {
 if [[ "$1" == "--help" ]]; then
   display_help
 fi
-while getopts ":c:" opt; do
+
+while getopts ":c:t:" opt; do
   case $opt in
     c)
-      arg_c="$OPTARG"
+      arg_cmd="$OPTARG"
+      ;;
+    t)
+      arg_t="$OPTARG"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -27,16 +47,25 @@ while getopts ":c:" opt; do
   esac
 done
 
-if [[ $arg_c ]]; then
+if [[ $arg_t ]]; then
+  bannertext=$arg_t
+elif [[ $arg_cmd ]]; then
+  bannertext=$arg_cmd
+fi
+
+bannertext= pretty_shorten_text "$long_text" "$columns"
+
+if [[ $bannertext ]]; then
   columns=$(tput cols) # Screen width
-  partLength=$((($columns - ${#arg_c}) / 2 - 1))
+  partLength=$((($columns - ${#bannertext}) / 2 - 1))
 
   part1=$(printf '=%.0s' $(seq 1 $partLength))
   part2=$(printf '=%.0s' $(seq 1 $partLength))
 
-  echo "$part1 $arg_c $part2"
-  exec $arg_c
+  echo "$part1 $bannertext $part2"
+  if [[ arg_cmd ]]; then
+    exec $bannertext
+  fi
 else # if no argument is given just print =====
   echo $(printf '=%.0s' $(seq 1 $(tput cols)))
 fi
-
